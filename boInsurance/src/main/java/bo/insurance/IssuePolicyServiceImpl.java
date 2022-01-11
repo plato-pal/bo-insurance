659265b77b5f79614620f55a1e9fe79b6bc6bc35
k <k>
2022-01-11 12:28:21 +0200

    Responds with null in case the RabbitMQ is not working properly

--- boInsurance/src/main/java/bo/insurance/IssuePolicyServiceImpl.java
+++ boInsurance/src/main/java/bo/insurance/IssuePolicyServiceImpl.java
@@ -1,6 +1,9 @@
 package bo.insurance;
 
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import org.apache.commons.lang3.StringUtils;
 
 import bo.insurance.messaging.CustomerMessage;
@@ -15,32 +18,36 @@ public class IssuePolicyServiceImpl implements IssuePolicyService {
 				issuePolicyRequest.getName(), issuePolicyRequest.getEmail());
 		
 		Policy policy = issuePolicy.doIssue();
-
-		sendToCustomerQueue(policy);
-		sendToCollectionsQueue(policy);
-		
+		try {
+			sendToCustomerQueue(policy);
+			sendToCollectionsQueue(policy);
+		}catch(Exception e) {
+			return null;
+		}
 		return new IssuePolicyResponse(policy.getNumber(), policy.getTotalPremium());
 	}
 
-	private void sendToCustomerQueue(Policy policy) {
+	private void sendToCustomerQueue(Policy policy) throws IOException, TimeoutException  {
 		if(!StringUtils.isEmpty(policy.getNumber()) && !StringUtils.isEmpty(policy.getCustomerName()) && StringUtils.isNotEmpty(policy.getCustomerEmail())){
 			CustomerMessage customerMessage = new CustomerMessage(policy.getCustomerName(), policy.getNumber(), policy.getCustomerEmail());
 			try {
 				PolicyMessagingService.sendMessage(customerMessage, CustomerMessage.CUSTOMER_QUEUE_NAME);
-			} catch (Exception e) {
+			}catch (IOException | TimeoutException e) {
 				System.err.println("The message could not be sent to Customer: " + e.getMessage());
+				throw e;
 			}
 		}
 	}
 	
-	private void sendToCollectionsQueue(Policy policy) {
+	private void sendToCollectionsQueue(Policy policy) throws IOException, TimeoutException {
 		if(!StringUtils.isEmpty(policy.getNumber()) && null != policy.getTotalPremium()){
 			CollectionsNewPolicyMessage collectionsNewPolicyMessage 
 			= new CollectionsNewPolicyMessage(policy.getNumber(), policy.getTotalPremium());
 			try {
 				PolicyMessagingService.sendMessage(collectionsNewPolicyMessage, CollectionsNewPolicyMessage.COLLECTIONS_QUEUE);
-			} catch (Exception e) {
+			}catch (IOException | TimeoutException e) {
 				System.err.println("The message could not be sent to collections: " + e.getMessage());
+				throw e;
 			}
 		}
 	}
